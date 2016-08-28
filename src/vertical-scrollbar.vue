5c3498a58681bda5c0aff9464a9b7fcf6aabbb66
Undrizzle <undrizzle@gmail.com>
2016-08-28 20:19:55 +0800

    add wheel event

---
+++ src/vertical-scrollbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="nice-bar-rail-y"></div>
+    <div class="nice-bar-slider-y" v-bind:style="{ 'height': height + 'px', top: scrolling.v + '%' }" v-bind:class="{ 'fade-in': show, 'fade-out': !show }" v-el: scroll-slider-y></div>
+</template>
+
+<script>
+    export default {
+        props: {
+            content: {
+                type: Object,
+                default: 0
+            },
+            container: {
+                type: Object,
+                default: 0
+            },
+            scrolling: {
+                type: Object,
+                default: { v: 0, h: 0 }
+            },
+            show: {
+                type: Boolean,
+                default: false
+            },
+            draggingFromParent: {
+                type: Boolean,
+                default: false
+            },
+            onChangePosition: {
+                type: Function,
+                default: () => {}
+            }
+        },
+
+        data() {
+            return {
+                height: 0,
+                dragging: false,
+                start: 0
+            }
+        },
+
+        methods: {
+            calculateSize() {
+                this.height = this.container.height * this.container.height / this.content.height
+            }
+        },
+
+        ready() {
+            this.calculateSize()
+        }
+    }
+</script>
--- src/vue-nice-scrollbar.vue
+++ src/vue-nice-scrollbar.vue
@@ -1,15 +1,20 @@
 <template>
     <div id="{{ ids }}" class="nice-bar" v-bind:class="{ 'theme-light': theme === 'light' , 'theme-drak': theme === 'dark' }" v-el:scroll-container>
-        <div class="" v-el:scroll-content v-bind="{ 'overflow': 'hidden', 'height': scrollContainerHeight + 'px' }">
+        <div class="" v-el:scroll-content v-bind:style="{ 'margin-top': top * -1 + 'px', 'margin-left': left * -1 + 'px' }" @wheel="scroll">
             <slot></slot>
         </div>
-        <div class="nice-bar-rail-y" v-if="verticalReady"></div>
+        <vertical-scrollbar v-if="ready" v-bind:content="{ height: scrollContentHeight }" v-bind:container="{ height: scrollContainerHeight }"
+                            v-bind:scrolling="{ v: vMovement }" v-bind:dragging-from-parent="dragging" v-bind:on-change-position="handleChangePosition"
+                            v-bind:show="show">
+        </vertical-scrollbar>
+     <!--   <div class="nice-bar-rail-y" v-if="verticalReady"></div>
         <div class="nice-bar-slider-y" v-if="verticalReady" v-bind:style="{ 'height': sliderYHeight + 'px' }" v-bind:class="{ 'fade-in': show, 'fade-out': !show }" v-el: scroll-slider-y></div>
+        -->
     </div>
 </template>
 
 <script>
-    import dom from './js/util/dom'
+    import verticalScrollbar from './vertical-scrollbar.vue'
 
     export default {
         props: {
@@ -20,20 +25,34 @@
             theme: {
                 type: String,
                 default: "light"
+            },
+            speed: {
+                type: Number,
+                default: 53
             }
         },
 
         data() {
             return {
-                verticalReady: false,
+                ready: false,
                 scrollContainerHeight: null,
                 scrollContainerWidth: null,
                 scrollContentHeight: null,
                 scrollContentWidth: null,
                 sliderYHeight: null,
                 show: false,
-                dragging: false
+                dragging: false,
+                scrollY: null,
+                scrollX: null,
+                top: 0,
+                left: 0,
+                vMovement: 0,
+                hMovement: 0
             }
+        },
+
+        components: {
+            verticalScrollbar
         },
 
         methods: {
@@ -51,8 +70,7 @@
 
                 this.sliderYHeight = this.scrollContainerHeight * this.scrollContainerHeight / this.scrollContentHeight
 
-                if (this.scrollContentHeight > this.scrollContainerHeight)
-                    this.verticalReady = true
+                this.ready = true
             },
 
             showSliderY() {
@@ -62,6 +80,62 @@
             hideSliderY() {
                 if (!this.dragging)
                     this.show = false
+            },
+
+            scroll(e) {
+                e.preventDefault()
+
+                let num = this.speed
+
+                let shifted = e.shiftKey
+
+                this.scrollY = e.deltaY > 0 ? num : -(num)
+                this.scrollX = e.deltaX > 0 ? num : -(num)
+
+                if(shifted && e.deltaX == 0) this.scrollX = e.deltaY > 0 ? num : -(num)
+
+                let nextY = this.top + this.scrollY
+                let nextX = this.left + this.scrollX
+
+                let canScrollY = this.scrollContentHeight > this.scrollContainerHeight
+                let canScrollX = this.scrollContainerWidth > this.scrollContainerWidth
+
+                if (canScrollY && !shifted) {
+                    this.normalizeVertical(nextY)
+                    this.moveTheScrollbar()
+                }
+
+                if (shifted && canScrollX) {
+                    this.normalizeHorizontal(nextX)
+                    this.moveTheScrollbar()
+                }
+            },
+
+            normalizeVertical(nextY) {
+                let lowerEnd = this.scrollContentHeight - this.scrollContainerHeight
+
+                if (nextY > lowerEnd)
+                    nextY = lowerEnd
+                else if (nextY < 0)
+                    nextY = 0
+
+                this.top = nextY
+            },
+
+            normalizeHorizontal(nextX) {
+                let rightEnd = this.scrollContentWidth - this.scrollContainerWidth
+
+                if (nextX > rightEnd)
+                    nextX = rightEnd
+                else if (next < 0)
+                    nextX = 0
+
+                this.left = next
+            },
+
+            moveTheScrollbar() {
+                this.vMovement = this.top / this.scrollContentHeight * 100
+                this.hMovement = this.left / this.scrollContentWidth * 100
             }
         },
 
@@ -70,7 +144,7 @@
 
             window.addEventListener('resize', this.calculateSize)
 
-            if (this.verticalReady) {
+            if (this.ready) {
                 this.$els.scrollContainer.addEventListener('mouseenter', this.showSliderY)
                 this.$els.scrollContainer.addEventListener('mouseleave', this.hideSliderY)
             }
